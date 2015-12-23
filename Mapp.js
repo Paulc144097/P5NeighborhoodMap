99d82f90eee13e4115740a9d801fcf246befa297
Paulc144097 <paul.chua77@Yahoo.com>
2015-12-23 13:56:03 -0700

    Add knockout.js; bindings

--- Mapp.js
+++ Mapp.js
@@ -2,10 +2,21 @@ var markerArray = [];
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+	var self = this;
+
+    map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.714232, lng: -73.9612889},
     zoom: 13
   });
+
+
+
+    this.get-city = function(){
+    	var currentCity = this.city();
+    	//do Something
+    }
+
+
 
 
    var marker = new google.maps.Marker({
@@ -14,9 +25,13 @@ function initMap() {
     animation: google.maps.Animation.DROP,
     title: 'Hello World!'
   });
+   setMarkers(markers);
    marker.addListener('click', toggleModal());
    marker.setMap(map);
+
+   setAllMap();
 }
+ko.applyBindings(new initMap());
 
 
 function toggleModal() {
@@ -24,6 +39,18 @@ function toggleModal() {
     marker.setAnimation(null);
   } else {
     marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+//Determines if markers should be visible
+//This function is passed in the knockout viewModel function
+function setAllMap() {
+  for (var i = 0; i < markers.length; i++) {
+    if(markers[i].boolTest === true) {
+    markers[i].holdMarker.setMap(map);
+    } else {
+    markers[i].holdMarker.setMap(null);
+    }
   }
 }
 
