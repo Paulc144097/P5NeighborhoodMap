ec80c246bd696cc1a3dfd078dcc90f3b392beb69
Paulc144097 <paul.chua77@Yahoo.com>
2016-01-02 17:56:18 -0700

    Search bar fixed, geocoder doesn't work

--- Mapp.js
+++ Mapp.js
@@ -1,56 +1,65 @@
-var markerArray = [];
-var map;
+// var markerArray = [];
+// var map;
+// var city;
+// var geocoder;
 
 function initMap() {
 	var self = this;
-
+    city = new google.maps.LatLng(40.783060, -73.971249);
     map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.714232, lng: -73.9612889},
+    center: {lat: 40.783060, lng:-73.971249},
     zoom: 13
   });
-
-
-
-    this.get-city = function(){
-    	var currentCity = this.city();
-    	//do Something
-    }
-
-
-
-
-   var marker = new google.maps.Marker({
-    position: {lat: 40.714232, lng: -73.9612889},
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: 'Hello World!'
+  
+  var geocoder = new google.maps.Geocoder();
+  
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
   });
-   setMarkers(markers);
-   marker.addListener('click', toggleModal());
-   marker.setMap(map);
 
-   setAllMap();
+  //  var marker = new google.maps.Marker({
+  //   position: city,
+  //   map: map,
+  //   animation: google.maps.Animation.DROP,
+  //   title: 'Hello World!'
+  // });
+  //  setMarkers(markers);
+  //  marker.addListener('click', toggleModal());
+  //  marker.setMap(map);
+
+  //  setAllMap();
 }
 ko.applyBindings(new initMap());
 
 
-function toggleModal() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
+function geocodeAddress(geocoder, resultsMap){
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address':address}, function(results, status){
+    if (status === google.maps.GeocoderStatus.OK){
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
+
 
 //Determines if markers should be visible
 //This function is passed in the knockout viewModel function
-function setAllMap() {
-  for (var i = 0; i < markers.length; i++) {
-    if(markers[i].boolTest === true) {
-    markers[i].holdMarker.setMap(map);
-    } else {
-    markers[i].holdMarker.setMap(null);
-    }
-  }
-}
+// function setAllMap() {
+//   for (var i = 0; i < markers.length; i++) {
+//     if(markers[i].boolTest === true) {
+//     markers[i].holdMarker.setMap(map);
+//     } else {
+//     markers[i].holdMarker.setMap(null);
+//     }
+//   }
+// }
+
+
+
 
